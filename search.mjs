8364182b3076ec2bf04aded10a675c0bba297d19
Soban Ali <sobanali19@yahoo.com>
2023-10-20 22:46:53 -0400

    Create search.mjs

---
+++ search.mjs
@@ -0,0 +1,27 @@
+//search = prompt("What album are you looking for?");
+
+const searchQuery = 
+{
+	q : kpop,
+	type: album
+};
+
+const queryString = new URLSearchParams(searchQuery);
+
+fetch("https://accounts.spotify.com/api/search",
+	{
+		method: "GET",
+		body:
+		{
+			queryString.toString();
+		},
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			"Authorization": "Basic " + btoa(id_and_secret_b64),
+		},
+	})
+	.then((r) => r.json())	//	access token info is in r.json
+	.then((r) => {
+		console.log("Response", r); // You will get JSON response here.
+	})
+	.catch((error) => console.error("Error", error));
