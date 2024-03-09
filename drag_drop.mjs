c5df9a3b5c63f72217ec750cd7911f4f6e70f30f
Hamza Abdi <abdiahamza2002@gmail.com>
2024-03-09 00:07:03 -0500

    Update drag_drop.mjs

--- drag_drop.mjs
+++ drag_drop.mjs
@@ -1,5 +1,6 @@
 export function drag_drop(container) {
   const draggables = container.querySelectorAll(".drag_item");
+  const containers = document.querySelectorAll("track_listing")
 
   draggables.forEach((draggable) => {
     draggable.addEventListener("dragstart", () => {
@@ -12,34 +13,30 @@ export function drag_drop(container) {
     });
   });
 
+
   container.addEventListener("dragover", (e) => {
     e.preventDefault();
     const afterElement = getDragAfterElement(container, e.clientY);
-    console.log(afterElement);
     const draggable = document.querySelector(".dragging");
     if (afterElement == null) {
       container.appendChild(draggable);
     } else {
       container.insertBefore(draggable, afterElement);
     }
-  });
+  })
+
 
   function getDragAfterElement(container, y) {
-    const draggableElements = [
-      ...container.querySelectorAll(".draggable:not(.dragging)"),
-    ];
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
-    return draggableElements.reduce(
-      (closest, child) => {
-        const box = child.getBoundingClientRect();
-        const offset = y - box.top - box.height / 2;
-        if (offset < 0 && offset > closest.offset) {
-          return { offset: offset, element: child };
-        } else {
-          return closest;
-        }
-      },
-      { offset: Number.NEGATIVE_INFINITY }
-    ).element;
-  }
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
 }
