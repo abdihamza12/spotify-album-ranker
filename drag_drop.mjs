ecb9444c89f82ffa732366e7b7b98f9163f55e33
Hamza Abdi <abdiahamza2002@gmail.com>
2024-03-09 19:12:30 -0500

    finishing up basic functionality of the ordering
    can now properly order songs in terms of favorite.

--- drag_drop.mjs
+++ drag_drop.mjs
@@ -17,26 +17,29 @@ export function drag_drop(container) {
   container.addEventListener("dragover", (e) => {
     e.preventDefault();
     const afterElement = getDragAfterElement(container, e.clientY);
+    console.log(afterElement)
     const draggable = document.querySelector(".dragging");
     if (afterElement == null) {
-      container.appendChild(draggable);
+      container.appendChild(draggable)
     } else {
-      container.insertBefore(draggable, afterElement);
+      container.insertBefore(draggable, afterElement)
     }
   })
 
 
-  function getDragAfterElement(container, y) {
-  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+function getDragAfterElement(track_list, y) {
+  const draggableElements = [...track_list.querySelectorAll('.drag_item:not(.dragging)')]
 
   return draggableElements.reduce((closest, child) => {
     const box = child.getBoundingClientRect()
-    const offset = y - box.top - box.height / 2
-    if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child }
+    const offset = y - box.top - box.height / 2;
+    // console.log(offset);
+    if(offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child}
     } else {
-      return closest
+      return closest;
     }
-  }, { offset: Number.NEGATIVE_INFINITY }).element
+  }, {offset: Number.NEGATIVE_INFINITY }).element
+  
 }
 }
