f1631255b4f09bcf1f222362f113e688bd0b211f
Hamza Abdi <abdiahamza2002@gmail.com>
2024-08-11 02:00:24 -0400

    delete client credentials

--- client_credentials.mjs
+++
@@ -1,2 +0,0 @@
-export const client_id = "d10044f2d95d49b09c36e3bec519c6de";
-export const client_secret = "22b31696ebf049988068d852bb4401df";
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const client_id = "d10044f2d95d49b09c36e3bec519c6de"
+import { client_id } from "./pull_env.mjs"
+
+
 const redirect_uri = 'https://abdihamza12.github.io/spotify-album-ranker/callback.html'
 
 var auth_link = 'https://accounts.spotify.com/authorize?client_id=' +
