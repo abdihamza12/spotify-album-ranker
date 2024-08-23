ebbda222c6044d7e7a003d1aa26b57c7e4202ef0
Hamza Abdi <101903292+abdihamza12@users.noreply.github.com>
2024-08-23 15:02:51 -0400

    add host name to server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ fetch("https://accounts.spotify.com/api/token", {
 .catch((error) => console.error("Error", error)); 
 }
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, "https://rankd-testing.vercel.app", () => {
     console.log("server is running on https://rankd-testing.vercel.app:3000/api")
 })
 
