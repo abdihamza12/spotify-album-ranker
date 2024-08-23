ed1a5e0f0eae2d1e8f5da2b950003f9eec3028b4
Hamza Abdi <101903292+abdihamza12@users.noreply.github.com>
2024-08-23 15:14:19 -0400

    changed port number

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ fetch("https://accounts.spotify.com/api/token", {
 .catch((error) => console.error("Error", error)); 
 }
 
-app.listen(process.env.PORT || 3030, "https://rankd-testing.vercel.app", () => {
+app.listen(process.env.PORT || 40000, "https://rankd-testing.vercel.app", () => {
     console.log("server is running on https://rankd-testing.vercel.app:3030/api")
 })
 
