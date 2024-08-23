f3cefd6a15185c18ae35284dd9df7f6f627b1f8b
Hamza Abdi <101903292+abdihamza12@users.noreply.github.com>
2024-08-23 15:37:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ const app = express();
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from the HamzaAPI!' });
+    res.send('Hello from the HamzaAPI!');
   });
 
-
-// app.use(express.json());
+app.post('/api/data', (req, res) => {
+    res.json({recievedData: req.body})
+})
 
 // app.post("/api/data", (req, res) => {
 //     const data = req.body
